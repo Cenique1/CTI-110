a9db43724c2aa7adb00d265caa1add5bf353e390
Cenique1 <robinsoc0678@student.faytechcc.edu>
2025-02-18 21:24:33 -0500

    Add files via upload
    First Admission

---
+++ P1LAB1_RobinsonCenique.py
@@ -0,0 +1,12 @@
+# Your Name
+print('Cenique Robinson')
+# Date 
+print('February 18th, 2025')
+# assignment name 
+print('P1LAB1')
+# This is a comment
+print('Introduction to Python assignment')
+
+first_name = input('Please enter your First Name: ')
+last_name = input('Please enter your Last Name: ')
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
